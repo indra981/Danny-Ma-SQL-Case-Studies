eb73425048887d47b0a568d7d2b4395549682ac4
indra981 <80574457+indra981@users.noreply.github.com>
2025-02-15 20:35:17 +0530

    CS_A_Customer Journey.sql

--- Case Study - Foodie-Fi/CS_A_Customer Journey.sql
+++ Case Study - Foodie-Fi/CS_A_Customer Journey.sql
@@ -1,8 +1,11 @@
-CSA - Customer Journey
+## CSA - Customer Journey
 
---Based off the 8 sample customers provided in the sample from the subscriptions table, write a brief description about each customer’s onboarding journey.
+### Requirements
+Based off the 8 sample customers provided in the sample from the subscriptions table, write a brief description about each customer’s onboarding journey.
 
---Customer 1
+***
+
+#### Customer 1
 SELECT customer_id,
        p.plan_id,
        plan_name,
@@ -12,10 +15,11 @@ JOIN plans p
 ON s.plan_id = p.plan_id
 WHERE customer_id =1;
 
---Customer started the free trial on August 1, 2020
---Subscribed to the basic monthly during the seven day the trial period (on August 8, 2020)
+### Observations
+Customer started the free trial on August 1, 2020
+Subscribed to the basic monthly during the seven day the trial period (on August 8, 2020)
 
---Customer 37
+#### Customer 37
 SELECT customer_id,
        s.plan_id,
        plan_name,
@@ -25,12 +29,12 @@ JOIN Plans p
 ON s.plan_id = p.plan_id
 WHERE customer_id =37;
 
---Customer started the free trial on August 5, 2020 
---Subscribed to the basic monthly during the seven day the trial period (on August 12, 2020)
---Upgraded to the pro monthly plan after 3 months (on November 11, 2020)
+### Observations
+Customer started the free trial on August 5, 2020 
+Subscribed to the basic monthly during the seven day the trial period (on August 12, 2020)
+Upgraded to the pro monthly plan after 3 months (on November 11, 2020)
 
-
---Customer 73
+#### Customer 73
 SELECT customer_id,
        s.plan_id,
        plan_name,
@@ -40,12 +44,13 @@ JOIN Plans p
 ON s.plan_id = p.plan_id
 WHERE customer_id =62;
 
---Customer started the free trial on October 12, 2020
---Subscribed to the basic monthly after the seven day the trial period (on October 19, 2020)
---Subscription upgraded to the pro monthly plan after 3 months (January 2, 2021)
---Cancelled the subscription after 1 month (Fabruary 23, 2021)
+### Observations
+Customer started the free trial on October 12, 2020
+Subscribed to the basic monthly after the seven day the trial period (on October 19, 2020)
+Subscription upgraded to the pro monthly plan after 3 months (January 2, 2021)
+Cancelled the subscription after 1 month (Fabruary 23, 2021)
 
---Customer 87
+#### Customer 87
 SELECT customer_id,
        s.plan_id,
        plan_name,
@@ -55,11 +60,12 @@ JOIN Plans p
 ON s.plan_id = p.plan_id
 WHERE customer_id =87;
 
---Customer started the free trial on August 8, 2020
---Subscribed to pro monthly after the seven day the trial period (on August 15, 2020)
---Upgraded to the pro annual plan in September 2020
+### Observations
+Customer started the free trial on August 8, 2020
+Subscribed to pro monthly after the seven day the trial period (on August 15, 2020)
+Upgraded to the pro annual plan in September 2020
 
---Customer 99
+#### Customer 99
 SELECT customer_id,
        s.plan_id,
        plan_name,
@@ -69,10 +75,11 @@ JOIN Plans p
 ON s.plan_id = p.plan_id
 WHERE customer_id =99;
 
---Customer started the free trial on December 5, 2020
---Subscription cancelled on the last day of the trial period.
+### Observations
+Customer started the free trial on December 5, 2020
+Subscription cancelled on the last day of the trial period.
 
-----Customer 299
+#### Customer 299
 SELECT customer_id,
        s.plan_id,
        plan_name,
@@ -82,10 +89,11 @@ JOIN Plans p
 ON s.plan_id = p.plan_id
 WHERE customer_id =299;
 
---Customer started the free trial on January 10, 2020
---Subscribed to the basic monthly plan during the seven day the trial period
+### Observations
+Customer started the free trial on January 10, 2020
+Subscribed to the basic monthly plan during the seven day the trial period
 
---Customer 542
+#### Customer 542
 SELECT customer_id,
        s.plan_id,
        plan_name,
@@ -95,11 +103,12 @@ JOIN Plans p
 ON s.plan_id = p.plan_id
 WHERE customer_id =542;
 
---Customer started the free trial on April 7, 2020
---Upgraded tl Pro Annual on seven day trail period (on Aprtl 14, 2020)
---Subscription cancelled exactly after 1 year (on April 14, 2021)
+### Observations
+Customer started the free trial on April 7, 2020
+Upgraded tl Pro Annual on seven day trail period (on Aprtl 14, 2020)
+Subscription cancelled exactly after 1 year (on April 14, 2021)
 
---Customer 999
+#### Customer 999
 SELECT customer_id,
        s.plan_id,
        plan_name,
@@ -109,6 +118,7 @@ JOIN Plans p
 ON s.plan_id = p.plan_id
 WHERE customer_id =999;
 
---Customer started the free trial on October 23, 2020
---Upgraded tl Pro Monthly on October 30, 2020
---Subscription cancelled on December 1, 2020
+### Observations
+Customer started the free trial on October 23, 2020
+Upgraded tl Pro Monthly on October 30, 2020
+Subscription cancelled on December 1, 2020
